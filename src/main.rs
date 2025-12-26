b9abcd27c109f1b94a6c67179701b6870ed53555
Yoran Mandema <yoranmandema@gmail.com>
2025-12-26 15:09:19 +0100

    Attempt at adding pratt parser for expressions

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,42 @@
-use crate::parser::parse_program;
+use crate::parser::{Statement, parse_program};
 
 mod parser;
 
+#[macro_use]
+extern crate lazy_static;
 
 fn main() {
-    let input = "test = wawd;";
+    let input = std::fs::read_to_string("examples/helloworld.mln")
+        .expect("Failed to read examples/helloworld.mln");
 
-    let res = parse_program(input).unwrap();
+    let res = parse_program(&input).unwrap();
 
     println!("{:?}", res);
+
+    for statement in res.statements {
+        match statement {
+            Statement::Assign {
+                identifier,
+                expression,
+            } => {
+                println!("Assigning {} to {:?}", identifier, expression);
+            }
+            Statement::AssignIncrement {
+                identifier,
+                expression,
+            } => {
+                println!("Incrementing {} by {:?}", identifier, expression);
+            }
+
+            Statement::AssignDecrement {
+                identifier,
+                expression,
+            } => {
+                println!("Decrementing {} by {:?}", identifier, expression);
+            }
+            Statement::Expression(expression) => {
+                println!("Expression: {:?}", expression);
+            }
+        }
+    }
 }
